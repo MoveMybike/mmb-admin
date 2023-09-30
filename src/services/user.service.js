f93b49ea0e5edf63258fa934a0fe8d8b1ceb945c
Suresh <movemybike.git@gmail.com>
2023-09-30 17:37:46 +0530

    commit for mmb-admin

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://localhost:8080/v1/admin/';
+const API_URL = process.env.REACT_APP_BACKEND_URL+'/v1/admin/';
 const user = JSON.parse(localStorage.getItem('user'));
 
 class UserService {
